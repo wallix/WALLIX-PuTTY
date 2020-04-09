d247d1ac28e45658629c22d923591a7e7a54c4d9
Raphaël ZHOU <rzhou@wallix.com>
2020-04-09 12:24:27 +0200

    Minor changes.

---
+++ windows/VS2015-WAB/iploop/ExecutableSigner.py
@@ -0,0 +1,22 @@
+import os.path
+import subprocess
+
+target_file_name           = 'Release\iploop.exe'
+sign_cert_file_name        = '..\..\SignCertFile.p12'
+sign_cert_passwd_file_name = '..\..\SignCertPasswdFile.txt'
+
+if not os.path.isfile(target_file_name):
+	print('not target_file_name');
+else:
+	print('target_file_name');
+	
+if os.path.isfile(target_file_name) and                                     \
+   os.path.isfile(sign_cert_file_name) and                                  \
+   os.path.isfile(sign_cert_passwd_file_name):
+    print('ExecutableSigner');
+    with open(sign_cert_passwd_file_name, 'r') as sign_cert_passwd_file:
+        sign_cert_passwd = sign_cert_passwd_file.read().replace('\n', '')
+        subprocess.call(                                                    \
+            ['signtool.exe', 'sign', '/t',                                  \
+             'http://timestamp.digicert.com', '/f', sign_cert_file_name,    \
+             '/fd', 'sha256', '/p', sign_cert_passwd, target_file_name])
